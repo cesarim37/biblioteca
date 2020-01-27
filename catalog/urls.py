017feefca330f5763045b44429f106a22902f31b
Cesar Karim <cesarim37@gmail.com>
2020-01-27 06:37:09 -0430

    permisologia para la gestion de libros y material no bibliografico

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@
 
 # Django
 from django.urls import path
-
+from django.contrib.auth.decorators import login_required
 # Views
 from catalog import views
 
@@ -21,102 +21,102 @@ urlpatterns = [
     ########## CRUD de Autor ##########
     path(
         route='listar_autor/',
-        view=views.ListadoAutorView.as_view(),
+        view=login_required(views.ListadoAutorView.as_view()),
         name='listar_autor'
     ),
     path(
         route='crear_autor/',
-        view=views.CrearAutorView.as_view(),
+        view=login_required(views.CrearAutorView.as_view()),
         name='crear_autor'
     ),
     path(
         route='editar_autor/<int:pk>/',
-        view=views.ActualizarAutorView.as_view(),
+        view=login_required(views.ActualizarAutorView.as_view()),
         name='editar_autor'
     ),
     path(
         route='eliminar_autor/<int:pk>/',
-        view=views.EliminarAutorView.as_view(),
+        view=login_required(views.EliminarAutorView.as_view()),
         name='eliminar_autor'
     ),
 
     ########## CRUD de Editorial ##########
     path(
         route='listar_editorial/',
-        view=views.ListadoEditorialView.as_view(),
+        view=login_required(views.ListadoEditorialView.as_view()),
         name='listar_editorial'
     ),
     path(
         route='crear_editorial/',
-        view=views.CrearEditorialView.as_view(),
+        view=login_required(views.CrearEditorialView.as_view()),
         name='crear_editorial'
     ),
     path(
         route='editar_editorial/<int:pk>/',
-        view=views.ActualizarEditorialView.as_view(),
+        view=login_required(views.ActualizarEditorialView.as_view()),
         name='editar_editorial'
     ),
     path(
         route='eliminar_editorial/<int:pk>/',
-        view=views.EliminarEditorialView.as_view(),
+        view=login_required(views.EliminarEditorialView.as_view()),
         name='eliminar_editorial'
     ),
 
     ########## CRUD de Ubicación ##########
     path(
         route='listar_ubicacion/',
-        view=views.ListadoUbicacionView.as_view(),
+        view=login_required(views.ListadoUbicacionView.as_view()),
         name='listar_ubicacion'
     ),
     path(
         route='crear_ubicacion/',
-        view=views.CrearUbicacionView.as_view(),
+        view=login_required(views.CrearUbicacionView.as_view()),
         name='crear_ubicacion'
     ),
     path(
         route='editar_ubicacion/<int:pk>/',
-        view=views.ActualizarUbicacionView.as_view(),
+        view=login_required(views.ActualizarUbicacionView.as_view()),
         name='editar_ubicacion'
     ),
     path(
         route='eliminar_ubicacion/<int:pk>/',
-        view=views.EliminarUbicacionView.as_view(),
+        view=login_required(views.EliminarUbicacionView.as_view()),
         name='eliminar_ubicacion'
     ),
 
     ########## CRUD de Libro ##########
     path(
         route='listar_libros/',
-        view=views.ListadoLibrosView.as_view(),
+        view=login_required(views.ListadoLibrosView.as_view()),
         name='listar_libros'
     ),
     path(
         route='crear_libro/',
-        view=views.CrearLibroView.as_view(),
+        view=login_required(views.CrearLibroView.as_view()),
         name='crear_libro'
     ),
     path(
         route='editar_libro/<int:pk>/',
-        view=views.ActualizarLibroView.as_view(),
+        view=login_required(views.ActualizarLibroView.as_view()),
         name='editar_libro'
     ),
     path(
         route='eliminar_libro/<int:pk>/',
-        view=views.EliminarLibroView.as_view(),
+        view=login_required(views.EliminarLibroView.as_view()),
         name='eliminar_libro'
     ),
 
     ########## Detalles de Libro ##########
     path(
         route='detalle_libro/<slug:slug>/<int:pk>/',
-        view= views.LibroDetailView.as_view(),
+        view=login_required(views.LibroDetailView.as_view()),
         name='libro_detail'
     ),
 
     ########## CRUD de Ejemplares ##########
     path(
-        route='crear_ejemplar/',
-        view=views.CrearEjemplarView.as_view(),
+        route='crear_ejemplar/<int:pk>/',
+        view=login_required(views.CrearEjemplarView.as_view()),
         name='crear_ejemplar'
     ),
 
@@ -127,36 +127,36 @@ urlpatterns = [
     ########## CRUD de Material ##########
     path(
         route='listar_material/',
-        view=views.ListadoMaterialView.as_view(),
+        view=login_required(views.ListadoMaterialView.as_view()),
         name='listar_material'
     ),
     path(
         route='crear_material/',
-        view=views.CrearMaterialView.as_view(),
+        view=login_required(views.CrearMaterialView.as_view()),
         name='crear_material'
     ),
     path(
         route='editar_material/<int:pk>/',
-        view=views.ActualizarMaterialView.as_view(),
+        view=login_required(views.ActualizarMaterialView.as_view()),
         name='editar_material'
     ),
     path(
         route='eliminar_material/<int:pk>/',
-        view=views.EliminarMaterialView.as_view(),
+        view=login_required(views.EliminarMaterialView.as_view()),
         name='eliminar_material'
     ),
 
     ########## Detalles de Material ##########
     path(
         route='detalle_material/<slug:slug>/<int:pk>/',
-        view= views.MaterialDetailView.as_view(),
+        view=login_required(views.MaterialDetailView.as_view()),
         name='material_detail'
     ),
 
     ########## CRUD de Ejemplares ##########
     path(
-        route='crear_ejemplar_material/',
-        view=views.CrearEjemplarMaterialView.as_view(),
+        route='crear_ejemplar_material/<int:pk>/',
+        view=login_required(views.CrearEjemplarMaterialView.as_view()),
         name='crear_ejemplar_material'
     ),
 ]
